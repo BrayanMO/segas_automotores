f16d0563b75a21dc6ffffb5b05339ead1d6575c8
Luis E. Morocho Febres <lmorochofebres@gmail.com>
2022-07-30 13:17:15 -0500

    Update adminModel.js
    add new routes, info account and update password

--- src/model/adminModel.js
+++ src/model/adminModel.js
@@ -119,4 +119,76 @@ module.exports = class adminModel {
       return res.json(parse);
     });
   }
+  
+  async updateAccount(req, res) {
+    const { id } = req.params;
+    const { firstName, lastName, email, phone, address } = req.body;
+
+    const query = `
+      update personal set ? where id_personal = ?
+    `;
+
+    const update = {
+      nombre: firstName,
+      apellido: lastName,
+      telef: phone,
+      email,
+      direccion: address,
+    };
+
+    db.query(query, [update, id], (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error al actualizar el usuario",
+          err,
+        });
+      }
+      return res.json({ message: "Usuario actualizado" });
+    });
+  }
+
+  async account(req, res) {
+    const { id } = req.params;
+    console.log(id);
+
+    const query = `
+      SELECT nombre, apellido, telef, email, direccion, id_login FROM personal WHERE id_login = ?
+    `;
+
+    db.query(query, [id], (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error al obtener la informacion del usuario",
+          err,
+        });
+      }
+      return res.json(rows);
+    });
+  }
+
+  async updatePassword(req, res) {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    const query = `
+      update login set ? where id_login = ?
+    `;
+
+    const update = {
+      password,
+    };
+
+    db.query(query, [update, id], (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error al actualizar la constraseña del usuario",
+          err,
+        });
+      }
+      return res.json({
+        message:
+          "Contraseña Actualizada correctamente, inicie sesion nuevamente",
+      });
+    });
+  }
 };
