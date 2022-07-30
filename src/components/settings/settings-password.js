3a3e0b5f0fbc3dfc4814e8be6a501b6f73fa9961
Luis2115 <lmorochofebres@gmail.com>
2022-07-30 13:28:31 -0500

    implementando funcionalidad a cambiar contraseña

--- src/components/settings/settings-password.js
+++ src/components/settings/settings-password.js
@@ -1,59 +1,85 @@
 import { useState } from "react";
 import { Box, Button, Card, CardContent, CardHeader, Divider, TextField } from "@mui/material";
+import { Form, Formik } from "formik";
+import { InputField } from "../FormFields";
+import * as Yup from "yup";
+import { updatePasswordApi } from "src/Api/AdminApi";
+import { useUSer } from "src/context/AuthContext";
+import { toast } from "react-toastify";
 
 export default function SettingsPassword(props) {
-  const [values, setValues] = useState({
-    password: "",
-    confirm: "",
-  });
+  const { user, logout } = useUSer();
 
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
+  function initialValues() {
+    return {
+      password: "",
+      confirm: "",
+    };
+  }
+
+  function validationSchema() {
+    return Yup.object({
+      password: Yup.string()
+        .min(6, "La contraseña debe tener al menos 6 caracteres")
+        .required("Este campo es requerido"),
+      confirm: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Las contraseñas no coinciden")
+        .required("Este campo es requerido"),
     });
+  }
+
+  const _handleSubmit = async (values, actions) => {
+    const response = await updatePasswordApi(user.idUser, values);
+
+    if (response.status === 200) {
+      toast.success(response.data.message);
+      logout();
+    } else toast.error("Error al actualizar la contraseña");
   };
 
   return (
-    <form {...props}>
-      <Card>
-        <CardHeader subheader="Actualiza tu Contraseña" title="Contraseña" />
-        <Divider />
-        <CardContent>
-          <TextField
-            fullWidth
-            label="Contraseña"
-            margin="normal"
-            name="password"
-            onChange={handleChange}
-            type="password"
-            value={values.password}
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Confirma la contraseña"
-            margin="normal"
-            name="confirm"
-            onChange={handleChange}
-            type="password"
-            value={values.confirm}
-            variant="outlined"
-          />
-        </CardContent>
-        <Divider />
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            p: 2,
-          }}
-        >
-          <Button color="primary" variant="contained">
-            Actualizar
-          </Button>
-        </Box>
-      </Card>
-    </form>
+    <Formik
+      initialValues={initialValues()}
+      validationSchema={validationSchema()}
+      onSubmit={_handleSubmit}
+    >
+      <Form>
+        <Card>
+          <CardHeader subheader="Actualiza tu Contraseña" title="Contraseña" />
+          <Divider />
+          <CardContent>
+            <InputField
+              name="password"
+              label="Contraseña"
+              type="password"
+              margin="normal"
+              fullWidth
+              autoComplete="new-password"
+            />
+
+            <InputField
+              name="confirm"
+              label="Confirmar Contraseña"
+              type="password"
+              margin="normal"
+              fullWidth
+              autoComplete="new-password"
+            />
+          </CardContent>
+          <Divider />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              p: 2,
+            }}
+          >
+            <Button type="submit" color="primary" variant="contained">
+              Actualizar
+            </Button>
+          </Box>
+        </Card>
+      </Form>
+    </Formik>
   );
 }
